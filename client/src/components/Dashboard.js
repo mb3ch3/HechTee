8121819ae46b6a134f1ccec184252b16e6410145
mb3ch3 <rhowdan@gmail.com>
2023-03-25 12:23:16 +0300

    fixing modal details for employees

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import axios from 'axios'
+import Modal from './Modal'
 
 
 class Dashboard extends React.Component {
@@ -9,13 +10,28 @@ class Dashboard extends React.Component {
             emps: [],
             gsts: [],
             showModal: false,
-            assigns : 0
+            assigns: 0,
+            id: '',
+            empname: '',
+            phone: '',
+            jobdes:'',
+            manager:''
 
         }
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.handleId = this.handleId.bind(this);
     }
-
+    handleId(idnumber,empname,phone,jobdes){
+        this.setState({id:idnumber})
+        this.setState({empname: empname})
+        this.setState({empname: empname})
+        this.setState({phone: phone})
+        this.setState({jobdes: jobdes})
+        console.log(empname);
+        console.log(phone);
+        console.log(jobdes);
+    }
     handleOpenModal() {
         this.setState({ showModal: true });
     }
@@ -56,35 +72,27 @@ class Dashboard extends React.Component {
             console.log(error)
         }
         try {
-                var w = 0
-                for (let index = 0; index < this.state.gsts.length; index++) {
-                    if (this.state.gsts[index].employee_id !== null) {
-                        w++
-                    }
-                    this.setState({assigns: w})
-                    console.log('This is it '+w)
+            var w = 0
+            for (let index = 0; index < this.state.gsts.length; index++) {
+                if (this.state.gsts[index].employee_id !== null) {
+                    w++
                 }
+                this.setState({ assigns: w })
+                console.log('This is it ' + w)
+            }
         } catch (error) {
             console.log(error);
         }
-        // const emplolist = []
-        // {
-        //     [...this.state.emps].map((item, index) => {
-        //         emplolist.push(<tr>
-        //             <td>{item.id}</td>
-        //             <td>{item.empname}</td>
-        //             <td>{item.phone}</td>
-        //             <td>{item.jobdes}</td>
-        //             <td>{item.admin_id}</td>
-        //         </tr>)
-        //         return (<div></div>)
-        //     })
-        // }
-        
     }
     render() {
         return (
             <div>
+                {
+                    this.state.showModal ? <div className="modal">
+                    <Modal name={this.state.empname} id={this.state.id} jobdes={this.state.jobdes} phone={this.state.phone} close={this.handleCloseModal} />
+                </div> : <div></div>
+                }
+                
                 <div className="bg" id="bg">
                     <div className='heading' id="heading" >
                         <span>Dashboard</span>
@@ -102,11 +110,11 @@ class Dashboard extends React.Component {
                         </div>
                         <div className="lead-card">
                             <div className="count">
-                           {
-                             this.state.gsts.filter(function(element){
-                                return element.employee_id !== null;
-                            }).length
-                           }
+                                {
+                                    this.state.gsts.filter(function (element) {
+                                        return element.employee_id !== null;
+                                    }).length
+                                }
                             </div>
                             <div className="object-count">Number of assignations</div>
                         </div>
@@ -129,7 +137,10 @@ class Dashboard extends React.Component {
                                 </thead>
                                 <tbody>
                                     {this.state.emps.map(item =>
-                                        <tr key={item.id}>
+                                        <tr key={item.id} onClick={()=>{
+                                            this.handleOpenModal()
+                                            this.handleId(item.id,item.empname,item.phone,item.jobdes)
+                                        }}>
                                             <td>{item.id}</td>
                                             <td>{item.empname}</td>
                                             <td>{item.phone}</td>
@@ -147,27 +158,27 @@ class Dashboard extends React.Component {
                             Guests
                         </div>
                         <div className="table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Name</th>
-                                    <th>Room</th>
-                                    <th>Employee assigned</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.gsts.map(item =>
-                                    <tr key={item.id}>
-                                        <td>{item.id}</td>
-                                        <td>{item.gstname}</td>
-                                        <td>{item.room}</td>
-                                        <td>{item.employee_id}</td>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Room</th>
+                                        <th>Employee assigned</th>
                                     </tr>
-                                )}
-                            </tbody>
-                        </table>
-                        </div> 
+                                </thead>
+                                <tbody>
+                                    {this.state.gsts.map(item =>
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td>{item.gstname}</td>
+                                            <td>{item.room}</td>
+                                            <td>{item.employee_id}</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,15 +2,15 @@ import '../App.css'
 import logo from '../assets/logo-white.png'
 function Modal(props){
     return(
-        <div className="modal-outer" onClick={()=>{console.log("clicked outer")}}>
+        <div className="modal-outer" onClick={props.close}>
             <div className="modal-inner">
                 <div className='modal-details'>
                     <div className='modal-title'>
-                        modal title
+                        {props.name}
                     </div>
-                    <div>modal detail 1</div>
-                    <div>modal detail 1</div>
-                    <div>modal detail 1</div>
+                    <div>{props.id}</div>
+                    <div>{props.phone}</div>
+                    <div>{props.jobdes}</div>
                 </div>
                 <div className='modal-image'>
                     <img src={logo} alt="logo here"/>
